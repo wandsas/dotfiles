61ee52ca07adb26c104a00be4b432d79e96ba062
Sascha Wander <sascha.wander@gmail.com>
2019-03-31 21:34:37 +0000

    repo

--- .zsh/rc.d/13-fast-syntax-highlighting.zsh
+++ .zsh/rc.d/13-fast-syntax-highlighting.zsh
@@ -1,7 +1,7 @@
 # Syntax Highlighting for the Z-Shell (https://github.com/zdharma/fast-syntax-highlighting)
 
 zshrc_fast_syntax_highlighting () {
-    . ~/.local/lib/fast-syntax-highlighting.plugin.zsh NIL || return
+    . ~/.local/lib/fast-syntax-highlighting.zsh NIL || return
     FAST_HIGHLIGHT[use_async]=1
     FAST_HIGHLIGHT[use_brackets]=1
 }
