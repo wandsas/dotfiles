2165aba1905c81c600216c6c17f9bb215fbb4ecb
Sascha Wander <sascha.wander@gmail.com>
2021-06-24 20:17:47 +0200

    f

--- .cfg-post.d/xdg-init.sh
+++ .cfg-post.d/xdg-init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Install pre requirements for user environment setup
+# dotfiles/.cfg-post.d
 
-# ansi colors
+
+# Ansi colors
 red="\e[0;91m"
 yellow="\e[0;93m"
+green="\e[0;92m"
 bold="\e[1m"
 reset="\e[0m"
 
@@ -14,7 +16,10 @@ XDG_DATA_LOCAL=$HOME/.local/share
 
 # Prepare XDG dirs for first user login
 for XDG in $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME; do
-    printf "${yellow}==>${reset} Preparing XDG_CONFIG_HOME=$XDG_CONFIG_HOME\n"
+    echo "xdg $XDG"
+done
+  
+printf "${yellow}==>${reset} XDG_CONFIG_HOME=$XDG_CONFIG_HOME\n"
     if [ -d $XDG ]; then
         printf "${yellow}==>${reset} Directory: $XDG still exists. Doing nothing.\n"
     else
@@ -23,5 +28,5 @@ for XDG in $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME; do
     fi
 done
 
-
-unset XDG_CONFIG XDG_CACHE_HOME XDG_DATA_HOME
+# Try to keep the environmenet pollution down, EPA loves us
+unset red yellow bold reset XDG_CONFIG XDG_CACHE_HOME XDG_DATA_HOME
