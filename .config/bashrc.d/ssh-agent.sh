97e586ac8aa7acf7727e9fe2f696f6f31fc2da82
Sascha Wander <sascha.wander@gmail.com>
2021-06-25 16:19:18 +0000

    rmssh-agent

--- .config/bashrc.d/ssh-agent.sh
+++
@@ -1,9 +0,0 @@
-envfile=$XDG_CACHE_HOME/ssh-agent-env
-if [ -r $envfile ]; then
-	while read line; do
-		export $line
-	done <$envfile
-else
-	eval export $(ssh-agent -s)
-	printf "GPG_AGENT_INFO=$GPG_AGENT_INFO\nSSH_AUTH_SOCK=$SSH_AUTH_SOCK\nSSH_AGENT_PID=$SSH_AGENT_PID\n" >$envfile
-fi
