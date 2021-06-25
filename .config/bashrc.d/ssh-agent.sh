c2498bd5483b999412fcca431fd3738c530de5a3
Sascha Wander <sascha.wander@gmail.com>
2021-06-25 16:18:01 +0000

    fixssh-agent

--- .config/bashrc.d/ssh-agent.sh
+++ .config/bashrc.d/ssh-agent.sh
@@ -4,7 +4,6 @@ if [ -r $envfile ]; then
 		export $line
 	done <$envfile
 else
-	eval export $(gpg-agent --homedir ~/.gnupg --use-standard-socke --daemon --sh)
 	eval export $(ssh-agent -s)
 	printf "GPG_AGENT_INFO=$GPG_AGENT_INFO\nSSH_AUTH_SOCK=$SSH_AUTH_SOCK\nSSH_AGENT_PID=$SSH_AGENT_PID\n" >$envfile
 fi
