0ba2919c52029e466be163a2695e31808bc47a78
Sascha Wander <sascha.wander@gmail.com>
2021-06-26 04:47:36 +0000

    androidtools

--- bin/install-sdk.sh
+++ bin/install-sdk.sh
@@ -1,29 +1,28 @@
 #!/bin/sh
 
+export ANDROID_HOME=$HOME/android/sdk
+
 sudo apt-get update
 sudo apt-get install openjdk-17-jdk curl unzip
 
-mkdir -p $HOME/android/sdk/cmdline-tools
-cd $HOME/android/sdk/cmdline-tools
-curl -O https://dl.google.com/android/repository/commandlinetools-linux-6514223_latest.zip
-echo 'ef319a5afdb41822cb1c88d93bc7c23b0af4fc670abca89ff0346ee6688da797 commandlinetools-linux-6514223_latest.zip' | sha256sum -c
-unzip commandlinetools-linux-6514223_latest.zip
-rm commandlinetools-linux-6514223_latest.zip
+# cmdline-tools
+[ -d $ANDROID_HOME/cmdline-tools ] || mkdir -p $ANDROID_HOME/cmdline-tools
+cd $ANDROID_HOME/cmdline-tools
+curl -O https://dl.google.com/android/repository/commandlinetools-linux-7302050_latest.zip
+echo '7a00faadc0864f78edd8f4908a629a46d622375cbe2e5814e82934aebecdb622 commandlinetools-7302050_latest.zip' | sha256sum -c
+unzip commandlinetools-linux-7302050_latest.zip
+rm commandlinetools-linux-7302050_latest.zip
 mv tools latest
-
-export ANDROID_HOME=$HOME/android/sdk
-export PATH=$HOME/android/sdk/cmdline-tools/latest/bin:$PATH
-
+export PATH=$ANDROID_HOME/cmdline-tools/latest/bin:$PATH
 sdkmanager 'cmdline-tools;latest'
+# platform-tools
 sdkmanager platform-tools
-
-export PATH=$HOME/android/sdk/platform-tools:$PATH
-
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+# build-tools
 sdkmanager 'build-tools;30.0.2'
 export PATH=$HOME/android/sdk/build-tools/30.0.2:$PATH
-
+# ndk-bundle
 sdkmanager ndk-bundle
-
-export PATH=$HOME/android/sdk/ndk-bundle:$PATH
+export PATH=$ANDROID_HOME/ndk-bundle:$PATH
 
 sdkmanager --update
