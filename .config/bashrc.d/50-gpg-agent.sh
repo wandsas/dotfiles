83c15bdaba6c2f282e4d97e2c5f260df7ba5d918
Sascha Wander <sascha.wander@gmail.com>
2021-07-12 03:50:05 +0200

    always source SSH_AUTH_SOCK

--- .config/bashrc.d/50-gpg-agent.sh
+++ .config/bashrc.d/50-gpg-agent.sh
@@ -1,7 +1,5 @@
 # 50-gpg-agent.sh
 
-if [ -z $SSH_AUTH_SOCK ]; then
-  export SSH_AUTH_SOCK=/run/user/$(id -i)/gnupg/S.gpg-agent.ssh
-fi
 export GPG_TTY=$(tty)
+export SSH_AUTH_SOCK=/run/user/$(id -i)/gnupg/S.gpg-agent.ssh
 echo UPDATESTARTUPTTY | gpg-connect-agent >/dev/null 2>&1
