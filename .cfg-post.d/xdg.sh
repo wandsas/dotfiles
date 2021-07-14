415ea835ed57ce0bf1a962287916c102f0a7504a
crosshatcher <crosshatcher@onionmail.org>
2021-07-14 23:51:41 +0000

    info

--- .cfg-post.d/xdg.sh
+++ .cfg-post.d/xdg.sh
@@ -2,10 +2,16 @@
 
 me=${0}
 
+msg () {
+  printf "\033[1m\033[0;93m=>\033[m $*\n" >&2
+}
+info () {
+  printf "\033[1m\033[0;93mINFO:\033[m $*\n" >&2
+}
 
 # Prepare xdg dirs for first user login.
 for xdg in ~/.config ~/.cache ~/.local/share; do
-    info "Testing $xdg."
+    msg "Testing $xdg."
 
     if [ -d $xdg ]; then
         info "Directory: $xdg exists. Abort."
