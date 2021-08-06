b95e118816312fdfdc6b831f436908eac4a75605
crosshatcher <crosshatcher@onionmail.com>
2021-08-06 09:42:42 +0000

    bashrc.d

--- .config/bashrc.d/10-perl5.sh
+++
@@ -1,5 +0,0 @@
-### ~/.config/bashrc.d/10-perl5.sh
-
-export PERL_LOCAL_LIB_ROOT="/home/wandsas/.local/lib/perl5"
-export PERL_MB_OPT="--install_base \"/home/wandsas/.local/lib/perl5\""
-export PERL_MM_OPT="INSTALL_BASE=/home/wandsas/.local/lib/perl5"
--- .config/bashrc.d/15-nvm.sh
+++
@@ -1,7 +0,0 @@
-# ~/.config/bashrc.d/15-nvm.sh
-
-if [ -d ~/.local/lib/nvm ]; then
-    export NVM_DIR=$HOME/.local/lib/nvm
-    [ -s $NVM_DIR/nvm.sh ] && . $NVM_DIR/nvm.sh
-    [ -s $NVM_DIR/bash_completion ] && . $NVM_DIR/bash_completion
-fi
